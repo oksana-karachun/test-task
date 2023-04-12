3292e58a1dede46446c17251d8f368b8e60d29f0
oksana.karachun <karachun.ok@gmail.com>
2023-04-12 20:29:27 +0200

    [TASK-3] add base router

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('obrio-test') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
--- src/pages/preload/index.tsx
+++ src/pages/preload/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Gender from './gender';
+import Parent from './parent';
+import Status from './status';
+import Problems from './problems';
+import Email from './signUp';
 
 import { Header } from 'widgets/header/Header';
 import { Footer } from 'widgets/footer/Footer';
 
 import styles from './index.module.scss';
+import { Route, Routes } from 'react-router-dom';
 
 const Preload = () => (
   <div className={styles.wrapper}>
     <Header />
-    <Gender />
+    <Routes>
+      <Route path={'/'} element={<Gender />} />
+      <Route path={'/status'} element={<Status />} />
+      <Route path={'/parent'} element={<Parent />} />
+      <Route path={'/problems'} element={<Problems />} />
+      <Route path={'/email'} element={<Email />} />
+    </Routes>
     <Footer />
   </div>
 );
