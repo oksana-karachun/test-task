1d2ab54d51d4779cb8fa13ca9223829e26e7f36e
oksana.karachun <karachun.ok@gmail.com>
2023-04-14 11:46:38 +0200

    [TASK-8] add display logic

--- src/pages/preload/relationship-info/index.selector.ts
+++ src/pages/preload/relationship-info/index.selector.ts
@@ -1,12 +1,23 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { createStructuredSelector } from 'reselect';
 
 import { RootState } from 'app/store';
 import { getZodiacSign } from 'shared/helpers';
 
-export const getZodiacSelector = createSelector(
+const zodiacSelector = createSelector(
   (state: RootState) => state.dataReducer.birth.day,
   (state: RootState) => state.dataReducer.birth.month,
   (day, month) => {
     return getZodiacSign(month, day);
   },
 );
+
+const userDecisionSelector = createSelector(
+  (state: RootState) => state.dataReducer.decisions,
+  (decision) => decision,
+);
+
+export const relationshipInfoSelector = createStructuredSelector({
+  zodiac: zodiacSelector,
+  userDecision: userDecisionSelector,
+});
