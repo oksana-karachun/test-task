48d0f08af4ca84129735e68b7c9a91ae40ba751e
oksana.karachun <karachun.ok@gmail.com>
2023-04-14 12:27:20 +0200

    [TASK-9] add pictures for purple page

--- src/app/icons/index.tsx
+++ src/app/icons/index.tsx
@@ -1,5 +1,10 @@
 import arrow from './arrow.svg';
 import chevron from './Chevron.svg';
+import circle from './circle.svg';
+import head from './head.svg';
+import heart from './heart.svg';
+import line from './line.svg';
 import logo_black from './logo_black.svg';
+import stars from './stars.svg';
 
-export { arrow, chevron, logo_black };
+export { arrow, chevron, circle, head, heart, line, logo_black, stars };
